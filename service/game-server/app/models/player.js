af4d36f38c0e346f4d468ddb05a170ce6ac81a31
caiqingdong <caiqingdong@qq.com>
2014-09-11 01:44:50 +0800

    添加玩家对象
    简单的添加玩家对象，待扩展

---
+++ service/game-server/app/models/player.js
@@ -0,0 +1,10 @@
+/**
+ * Created by caiqingdong on 14-9-11.
+ */
+
+var Player = function(opts){
+    this.id = opts.id;
+    this.name = opts.name;
+};
+
+module.exports = Player;
--- service/game-server/app/servers/login/handler/loginHandler.js
+++ service/game-server/app/servers/login/handler/loginHandler.js
@@ -2,6 +2,8 @@
  * Created by cqd on 14-9-2.
  */
 
+var Player = require("../../../models/player");
+
 module.exports = function(app) {
     return new Handler(app);
 };
@@ -13,11 +15,22 @@ var Handler = function(app) {
 
 var handler = Handler.prototype;
 
+handler.players = {};
+
 handler.login = function(msg, session, next)
 {
+    var player = this.players[msg.name];
+    if (!!player){
+        next(null, {code:200,msg:"已登录"});
+        return ;
+    }
+
     if (msg.name == msg.passwd)
     {
         next(null, {code:200,msg:"登陆成功"});
+
+        player = new Player({id:0, name:msg.name});
+        this.players[msg.name] = player;
         return;
     }
     next(null, {code:500,msg:"登陆失败"});
